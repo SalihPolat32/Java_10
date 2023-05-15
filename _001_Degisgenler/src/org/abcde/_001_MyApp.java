e31447ee133b0a656bfc6a134809f5622de6adca
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-05-15 17:15:50 +0300

    Add files via upload

---
+++ _001_Degisgenler/src/org/abcde/_001_MyApp.java
@@ -0,0 +1,72 @@
+/*
+ * Lisans Bilgileri Her Zaman Üstte Yazılır.
+ * 
+ */
+package org.abcde;
+
+import javax.sound.midi.SysexMessage;
+
+/**
+ * @author Salih Polat
+ * @since 2023
+ * @version 1.0.0
+ */
+public class _001_MyApp {
+
+	/**
+	 * Buraya Şekilde Çok Satırlı Not Yazılabilir.
+	 */
+	
+	public _001_MyApp() {
+		// Hazırlayıcı, Yapıcı
+		// TODO Yapılacak İşlerin Notudur.
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// İlk İş Yapan Yer
+		// TODO Auto-generated method stub
+		// String Yazılar İçin Söylenir.
+		// Chracter Sadece Bir Tane için Olur.
+		
+		System.out.println("Merhaba Java");
+		
+		System.out.println("Merhaba "+"Java");
+		
+		System.out.println("Merhaba"+" "+"Java");
+		
+		System.out.println("Merhaba"+' '+"Java");
+		
+		System.out.println("args DEĞERİ: "+args);
+		
+		System.out.println("null DEĞERİ: "+null);
+		
+		System.err.println("MESAJ GÖSTER");
+		
+		System.out.println("MyApp.main()");
+		
+		System.out.println(1+3);
+		
+		System.out.println(1+1);
+		
+		System.out.println("1"+"1");
+		
+		System.out.println("1"+1);
+		
+		System.out.println(1+"1");
+		
+		System.out.println('1'+1);
+		
+		System.out.println(1+'1');
+		
+		System.out.println('1'+'1');
+		
+		System.out.println('A'+'B');
+		
+		System.out.println('A'+"B");
+
+	}
+
+}
---
+++ _001_Degisgenler/src/org/abcde/_003_DegisgenTipleri.java
@@ -0,0 +1,97 @@
+package org.abcde;
+
+public class _003_DegisgenTipleri {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Primitive Types - İlkel Tipler
+		  
+		  int     - 1
+		  byte    - 8 bit
+		  short   -
+		  long    -
+		  float   -
+		  double  -
+		  boolean - 
+		  char    -
+		  
+		 */
+		/* Class Types - Sınıf Tipleri
+		  
+		  Sayısal
+		  Yazı
+		 */
+		
+		int myInt= 10;
+		byte myByte= 10;
+		short myShort= 10;
+		long myLong= 10;
+		float myFloat= 10;
+		double myDouble= 10;
+		boolean myBoolean= true;
+		char myChar= 'A';
+		  
+		System.out.println("int tipi: "+ myInt);
+		System.out.println("byte tipi: "+ myByte);
+		System.out.println("short tipi: "+ myShort);
+		System.out.println("long tipi: "+ myLong);
+		System.out.println("float tipi: "+ myFloat);
+		System.out.println("double tipi: "+ myDouble);
+		System.out.println("bloolean tipi: "+ myBoolean);
+		System.out.println("char tipi: "+ myChar);
+		
+		System.out.println("---------------------");
+				
+		Integer myInt2= 10;
+		Byte myByte2= 10;
+	    Short myShort2= 10;
+		Long myLong2= 10L;
+		Float myFloat2= 10F;
+		Double myDouble2= 10D;
+		Boolean myBoolean2= true;
+		Character myChar2= 'A';
+		
+		String myString= "Merhaba Java";
+		  
+		System.out.println("Integer tipi: "+ myInt2);
+		System.out.println("Byte tipi: "+ myByte2);
+		System.out.println("Short tipi: "+ myShort2);
+		System.out.println("Long tipi: "+ myLong2);
+		System.out.println("Float tipi: "+ myFloat2);
+		System.out.println("Double tipi: "+ myDouble2);
+		System.out.println("Bloolean tipi: "+ myBoolean2);
+		System.out.println("Character tipi: "+ myChar2);
+		
+		System.out.println("String tipi: "+ myString);
+		
+		System.out.println("---------------------");
+		
+		System.out.println("Integer tipi min: "+ myInt2.MIN_VALUE);
+		System.out.println("Integer tipi max: "+ myInt2.MAX_VALUE);
+		
+		System.out.println("Byte tipi min: "+ myByte2.MIN_VALUE);
+		System.out.println("Byte tipi max: "+ myByte2.MAX_VALUE);
+		
+		System.out.println("Short tipi min: "+ myShort2.MIN_VALUE);
+		System.out.println("Short tipi max: "+ myShort2.MAX_VALUE);
+		
+		System.out.println("Long tipi min: "+ myLong2.MIN_VALUE);
+		System.out.println("Long tipi max: "+ myLong2.MAX_VALUE);
+		
+		System.out.println("Float tipi min: "+ myFloat2.MIN_VALUE);
+		System.out.println("Float tipi max: "+ myFloat2.MAX_VALUE);
+		
+		System.out.println("Double tipi min: "+ myDouble2.MIN_VALUE);
+		System.out.println("Double tipi max: "+ myDouble2.MAX_VALUE);
+		
+		System.out.println("Bloolean tipi min: "+ myBoolean2.FALSE);
+		System.out.println("Bloolean tipi max: "+ myBoolean2.TRUE);
+		
+		System.out.println("Character tipi min: "+ myChar2.MIN_VALUE);
+		System.out.println("Character tipi max: "+ myChar2.MAX_VALUE);
+		
+		System.out.println("String tipi: "+ myString.length());
+	}
+
+}
