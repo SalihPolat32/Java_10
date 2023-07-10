b3d06ff2c3233ff6fd68f21077d48e54cd7e59c0
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-07-10 17:54:11 +0300

    10.07.2023 Gün Sonu

--- _058_SQL_StructuredQueryLanguage/src/main/resources/Day_02_WHERE_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/2023_07_05_sorgular.sql
+++ _058_SQL_StructuredQueryLanguage/src/main/resources/Day_02_WHERE_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/2023_07_05_sorgular.sql
@@ -1,75 +1,95 @@
-SELECT * FROM customers
+SELECT *
+FROM customers
 WHERE country = 'DE';
 
-SELECT * FROM customers;
-select * from customers;
-selecT * From customers;
+SELECT *
+FROM customers;
+select *
+from customers;
+selecT *
+From customers;
 
 -- Ülkesi Almanya Olmayanlar
-SELECT * FROM customers
+SELECT *
+FROM customers
 WHERE NOT country = 'DE';
 
 -- Ülkelerin İsimlerini Unique Tek Olarak Listeleyelim
-SELECT DISTINCT country FROM customers;
+SELECT DISTINCT country
+FROM customers;
 
 -- Yaşı 25'den Büyük Olanları Göster
 -- SELECT * FROM customers
 -- WHERE age > 25;
 
 -- Ülkelerin İsimlerini Unique Tek Olarak Listeleyelim ve Sayılarını da Gösterelim
-SELECT DISTINCT country FROM customers;
-SELECT COUNT (DISTINCT country) FROM customers;
+SELECT DISTINCT country
+FROM customers;
+SELECT COUNT(DISTINCT country)
+FROM customers;
 
 -- Alias (Lakap Takmak)
-SELECT first_name AS fname FROM customers;
+SELECT first_name AS fname
+FROM customers;
 
 -- Alias (Lakap Takmak)
-SELECT customer_id AS c_id, first_name AS c_name FROM customers;
+SELECT customer_id AS c_id, first_name AS c_name
+FROM customers;
 
 -- Alias (Lakap Takmak)
-SELECT customers.customer_id , customers.first_name FROM customers;
+SELECT customers.customer_id, customers.first_name
+FROM customers;
 
-SELECT c.customer_id , c.first_name FROM customers AS c;
+SELECT c.customer_id, c.first_name
+FROM customers AS c;
 
-SELECT c.customer_id , c.first_name FROM customers c;
+SELECT c.customer_id, c.first_name
+FROM customers c;
 
 -- SELECT o.id FROM order o;
 -- SELECT s.id FROM shp s;
 
-SELECT CONCAT (first_name, ' ', last_name) AS full_name FROM customers;
-SELECT CONCAT (first_name, ' ', last_name) full_name FROM customers;
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM customers;
+SELECT CONCAT(first_name, ' ', last_name) full_name
+FROM customers;
 
 -- Tabloya Yeni Bir Kayıt Ekleyelim
 INSERT INTO customers (first_name, last_name, phone, country, age)
-VALUES                ('AAA'     , 'BBB'    , '111', 'TR'   , 25);
+VALUES ('AAA', 'BBB', '111', 'TR', 25);
 
 -- INSERT INTO customers (customer_id, first_name, last_name, phone, country, age)
 -- VALUES                (20         ,'ASLAN'    , 'MİMAR'  , '999', 'CA'   , 40);
 
 INSERT INTO customers (first_name, last_name, phone, country, age)
-VALUES                ('AA1'     , 'BBB'    , '111', 'TR'   , 38),
-                      ('AA2'     , 'BBB'    , '111', 'TR'   , 42),
-					  ('AA3'     , 'BBB'    , '111', 'TR'   , 35);
-		
-SELECT * FROM customers;
+VALUES ('AA1', 'BBB', '111', 'TR', 38),
+       ('AA2', 'BBB', '111', 'TR', 42),
+       ('AA3', 'BBB', '111', 'TR', 35);
+
+SELECT *
+FROM customers;
 
 -- Sakın Böyle id Değeri Belirtilmeyen Update Yapmayın
 
-UPDATE customers 
+UPDATE customers
 SET age = 22;
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- Güncelleme, Edit, Update Önce id Değerini belirtmemiz Lazım customer_id = 16
-UPDATE customers 
+UPDATE customers
 SET age = 26
 Where customer_id = 1;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
-SELECT * FROM customers
+SELECT *
+FROM customers
 Where customer_id = 1;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- id Değeri 16'dan Büyük Olan Kişilerin Ülke Kodlarını Polonya (PL) Yapalım
 UPDATE customers
@@ -77,71 +97,91 @@ SET country = 'PL'
 WHERE customer_id > 16;
 -- WHERE customer_id >= 16;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- Silme İşleminde Mutlaka Silinecek Kaydın id Değerini Seçiniz. Yoksa Tüm Kaydı Sileriz.
 -- DELETE * FROM customers; -- Bunu Sakın Çalıştırma. Bu Tablodaki Tüm Verileri Siler.
 
 -- id Değeri 16'dan Büyükleri Olan Kaydı Silelim
-DELETE FROM customers
+DELETE
+FROM customers
 WHERE customer_id > 16;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- Tabloyu Fabrika Çıkışlı Haline Getirir. Her Şeyi Sıfırlar.
 -- TRUNCATE TABLE customers;
 
 -- Sıralama İçin
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY first_name;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- ASC A'dan Z'ye Sıralama, Yazmak Zorunda Değiliz. Varsayılan Olarak Var.
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY first_name ASC;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- DESC Z'den A'ya Sıralama, Yazmak Zorundayız. Tersten Sıralama Yapar.
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY first_name DESC;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY customer_id;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- Birden Fazla Durumla Sıralama
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY first_name, last_name, age;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- DESC Çoklu Sıralama
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY country DESC, first_name DESC;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- DESC İle Sadece first_name Sıralanır. country ASC İle Sıralanır. Yazılmasa da ASC Default Vardır.
-SELECT * FROM customers
+SELECT *
+FROM customers
 ORDER BY country, first_name DESC;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 -- Ülke Adı (Kişi Sayısı) Gruplama. Gruplamalarda 1 Kolonu Seçiniz.
-SELECT country, COUNT(*) AS numbers FROM customers
+SELECT country, COUNT(*) AS numbers
+FROM customers
 GROUP BY country;
 
 -- Grubun Şartı HAVING
-SELECT country,  COUNT(*) AS numbers FROM customers
+SELECT country, COUNT(*) AS numbers
+FROM customers
 
 -- WHERE age > 23
 GROUP BY country
 HAVING country = 'TR';
 
-SELECT * FROM customers
+SELECT *
+FROM customers
 WHERE country = 'TR';
--- _058_SQL_StructuredQueryLanguage/src/main/resources/Day_04_INNERJOIN_LEFTJOIN_RIGHTJOIN_FULLOUTERJOIN_UNION/2023_07_07_sorgular.sql
+++ _058_SQL_StructuredQueryLanguage/src/main/resources/Day_04_INNERJOIN_LEFTJOIN_RIGHTJOIN_FULLOUTERJOIN_UNION/2023_07_07_sorgular.sql
@@ -1,3 +1,68 @@
 --  SQL (INNER JOIN, LEFT JOIN RIGHT JOIN, FULL OUTER JOIN) UNION 
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
+
+SELECT *
+FROM orders;
+
+SELECT *
+FROM shippings;
+
+/*
+ * SELECT *
+ * FROM Tablo1
+ * JOIN Tablo2
+ * ON Şart
+ */
+
+-- Bütün Kolonları(Sütunları) Dahil Etti. Bu Babadan Kalma, Klasik JOIN'dir
+SELECT *
+FROM customers
+         JOIN orders
+              ON orders.customer_id = customers.customer_id;
+
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers AS c
+         INNER JOIN orders AS o
+                    ON o.customer_id = c.customer_id;
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers c
+         INNER JOIN orders o
+                    ON o.customer_id = c.customer_id;
+
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers c
+         LEFT JOIN orders o
+                   ON o.customer_id = c.customer_id;
+
+
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers c
+         RIGHT JOIN orders o
+                    ON o.customer_id = c.customer_id;
+
+
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers c
+         RIGHT JOIN orders o
+                    ON o.customer_id = c.customer_id
+WHERE o.amount > 500;
+
+
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers c
+         LEFT JOIN orders o
+                   ON o.customer_id = c.customer_id
+WHERE o.amount > 500;
+
+SELECT c.customer_id, c.first_name, c.last_name, o.item, o.amount, o.customer_id
+FROM customers c
+         FULL OUTER JOIN orders o
+                         ON c.customer_id = o.customer_id;
