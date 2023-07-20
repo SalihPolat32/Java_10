4906e942cfcdfc8ff5835fdad4969981720512fe
Salih Polat <97470567+SalihPolat32@users.noreply.github.com>
2023-07-20 16:34:47 +0300

    20.07.2023 Gün Sonu

---
+++ _070_HQL_HibernateQueryLanguage_CRUD/src/main/java/com/salihpolat/dao_repository/StudentDao.java
@@ -0,0 +1,38 @@
+package com.salihpolat.dao_repository;
+
+import com.salihpolat.model_entity.Student;
+import com.salihpolat.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class StudentDao {
+    public void saveStudent(Student student) {
+
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+            session.save(student);
+            transaction.commit();
+
+        } catch (Exception e) {
+
+            System.out.println("Hata: "+ e);
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+
+        }
+    }
+
+    public List<Student> getStudents() {
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Student", Student.class).list();
+        }
+    }
+}
---
+++ _070_HQL_HibernateQueryLanguage_CRUD/src/main/java/com/salihpolat/model_entity/Teacher.java
@@ -0,0 +1,14 @@
+package com.salihpolat.model_entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TEACHER")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column (name = "TEACHER_ID")
+    private String id;
+    private String firstname;
+}
